367266997fbf73fd54989abdd4bc49cf85e8f1ff
TheLazyEyeGuy <72226219+TheLazyEyeGuy@users.noreply.github.com>
2022-03-29 12:31:08 -0400

    updated dash
    added buttons for the coin selector page and a button to go back to dash, just add the filename to the coin select button

--- dashboard.php
+++ dashboard.php
@@ -23,6 +23,10 @@ if ($login) {;
         you are logged in as 
         <br>
         <input type="button" onclick="window.location.href='http://localhost/change_password.php';" value="change password" />
+        use these for the coin selecter page to go there and back respectively
+        <input type="button" onclick="window.location.href='http://localhost/adress';" value="change tracked coins" />
+        <input type="button" onclick="window.location.href='http://localhost/dashboard.php';" value="return to dashboard" />
+
     </form>
   </body>
 </html>
